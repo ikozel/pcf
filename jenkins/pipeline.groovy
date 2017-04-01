f33359d1d251a2ff1c451c09e69635f791da1a91
Igor Kozel <igorkozel@ip-192-168-1-10.ec2.internal>
2017-04-01 11:01:42 -0700

    init

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -12,12 +12,12 @@ pipelineJob("PCF/Infra/PCF-Environment") {
     def total_space_memory = ['2G','3G']
     def space_routs = ['2','4']
     def service_instances = ['2','4']
-    choiceParam('org_name', services, 'Service pipeline')
-    choiceParam('env_name', environments)
-    choiceParam('instance_memory', instance_memory)
-    choiceParam('total_space_memory', total_space_memory)
-    choiceParam('space_routs', space_routs)
-    choiceParam('service_instances', service_instances)
+    choiceParam('org_name', services, 'Service Pipeline')
+    choiceParam('env_name', environments, 'Stage Environment')
+    choiceParam('instance_memory', instance_memory, 'Instance Capacity')
+    choiceParam('total_space_memory', total_space_memory, 'Space Capacity')
+    choiceParam('space_routs', space_routs, 'Routs Capacity')
+    choiceParam('service_instances', service_instances, 'Service Capacity')
   }
 
   definition {

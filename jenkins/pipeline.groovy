fb691c8d8f1838f8cdd7e8ac4ce2f88950c1b2b6
Igor Kozel <igorkozel@ip-192-168-1-10.ec2.internal>
2017-04-01 10:05:56 -0700

    init

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -8,8 +8,16 @@ pipelineJob("PCF/Infra/environment") {
   parameters {
     def services = ['AppX','ServiceY', 'ComponentZ']
     def environments = ['cicd','dev','qa','stg','prod']
-    choiceParam('service', services)
-    choiceParam('environment', environments)
+    def instance_memory = ['1G','2G']
+    def total_space_memory = ['2G','3G']
+    def space_routs = ['2','4']
+    def service_instances = ['2','4']
+    choiceParam('org_name', services)
+    choiceParam('env_name', environments)
+    choiceParam('instance_memory', instance_memory)
+    choiceParam('total_space_memory', total_space_memory)
+    choiceParam('space_routs', space_routs)
+    choiceParam('service_instances', service_instances)
   }
 
   definition {
@@ -20,7 +28,7 @@ pipelineJob("PCF/Infra/environment") {
         git {
           remote {
             credentials("")
-            url("ps://github.com/ikozel/pcf.git")
+            url("https://github.com/ikozel/pcf.git")
             branch("*/master")
           }
         }

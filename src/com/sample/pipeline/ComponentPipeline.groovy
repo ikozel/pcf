3ef6501ef1a7da1d3ced926006b0f9d0e0b911fb
Igor Kozel <igor_kozel@yahoo.com>
2017-07-19 15:23:29 -0700

    Update ComponentPipeline.groovy

--- src/com/sample/pipeline/ComponentPipeline.groovy
+++ src/com/sample/pipeline/ComponentPipeline.groovy
@@ -3,7 +3,7 @@ package com.sample.pipeline
 
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 import org.jenkinsci.plugins.workflow.support.steps.input.Rejection
-import org.junit.Assert
+//import org.junit.Assert
 import groovy.json.*
 
 class ComponentPipeline implements Serializable {

0489f07fc9534efa74c4a16ef495fba09224cb9f
Igor Kozel <igor_kozel@yahoo.com>
2017-07-19 15:12:08 -0700

    Update ComponentPipeline.groovy

--- jenkins/src/com/sample/pipeline/ComponentPipeline.groovy
+++ jenkins/src/com/sample/pipeline/ComponentPipeline.groovy
@@ -6,19 +6,10 @@ import org.junit.Assert
 import groovy.json.*
 
 class ComponentPipeline implements Serializable {
-  def stages, env, options, environments, aws_profile, args
-  def componentName, release_version, environment_version
-  def deployed_environments, repoPath, cookbook_version
-  PipelineUtilities pipelineUtilities
-  ProvisioningComponentPipeline(Map args=[:], stages, env) {
+  def stages, args
+  
+  ComponentPipeline(Map args=[:], stages, env) {
     this.args                  = args
     this.stages                = stages
-    this.env                   = env
-    this.options               = options
-    this.componentName         = env.component_name
-    this.environment_version   = env.environment_version
-    this.deployed_environments = ""
-    this.repoPath              = ""
-    this.cookbook_version      = ""
   }
 }
